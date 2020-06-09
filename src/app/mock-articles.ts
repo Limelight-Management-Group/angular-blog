b8d40757b3144d499cd7efd684b0aee874091350
waynebanks <thestorerun@yahoo.com>
2020-06-09 12:34:50 -0700

    added classes to article component elements to target and style with css

--- src/app/mock-articles.ts
+++ src/app/mock-articles.ts
@@ -4,21 +4,21 @@ export const ARTICLES: Article[] = [
     {
         id: 1,
         title: 'first post. Check it out!',
-        content: '',
+        content: "I'm baby bicycle rights sartorial letterpress squid crucifix tousled selfies coloring book. Direct trade sustainable scenester, vinyl 8-bit beard +1 aesthetic freegan selfies. Pug chambray raw denim offal, yuccie tofu cold-pressed. Offal farm-to-table tacos vaporware literally food truck, cray paleo jianbing vexillologist four loko microdosing tumblr snackwave tilde.",
         description: 'You have to read this!',
         key: 'my-first-post',
         date: new Date(),
-        imageUrl: 'https://images.app.goo.gl/txA1VdGhk6BL7NHS6',
+        imageUrl: 'https://images.app.goo.gl/txA1VdGhk6BL7NHS6'
 
     },
     {
         id: 2,
         title: 'second post. Check it out!',
-        content: '',
+        content: "I'm baby bicycle rights sartorial letterpress squid crucifix tousled selfies coloring book. Direct trade sustainable scenester, vinyl 8-bit beard +1 aesthetic freegan selfies. Pug chambray raw denim offal, yuccie tofu cold-pressed. Offal farm-to-table tacos vaporware literally food truck, cray paleo jianbing vexillologist four loko microdosing tumblr snackwave tilde.",
         description: 'You have to read this on too!',
         key: 'my-second-post',
         date: new Date(),
-        imageUrl: 'https://images.app.goo.gl/txA1VdGhk6BL7NHS6',
+        imageUrl: 'https://images.app.goo.gl/txA1VdGhk6BL7NHS6'
         
     }    
 ]
